8ce6ef76908352019bdaf6107b347143409f0ce4
Thomas Byrnes <thomasbyrnes02@gmail.com>
2022-10-19 14:16:02 -0400

    added boilerplate code and files

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const configRoutes = require('./routes');
+
+app.use(express.json());
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log('Server running on: http://localhost:3000');
+});
